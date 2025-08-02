aa860c4c3baf343d86ebe0e3e8521cc22667da37
samaditya jatar <samadityaj@gmail.com>
2025-08-03 02:45:52 +0530

    Configure settings for Railway deployment

--- linkhub/settings.py
+++ linkhub/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
