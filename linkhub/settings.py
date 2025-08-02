c37733b661b3e22df7a06cbc576d571d26d8814c
samaditya jatar <samadityaj@gmail.com>
2025-08-03 02:39:56 +0530

    Configure settings for Railway deployment

--- linkhub/settings.py
+++ linkhub/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(' ')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-aiwzz86nc6&*5k5w2idt@ddf&ofyu93m+2^*m9ut11@u4gj4=@'
@@ -75,15 +81,9 @@ WSGI_APPLICATION = 'linkhub.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'linkhub_db',
-        'USER': 'postgres',
-        'PASSWORD': 'shivasama',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    'default': dj_database_url.config(conn_max_age=600)
     }
-}
+
 
 
 # Password validation
